f282bb6e06283cfe06acd20fc001136f0c7091fd
FedeIII <federico.perez@coverwallet.com>
2020-05-13 15:44:58 +0200

    dynamic font size

--- src/station-status.cs
+++ src/station-status.cs
@@ -1,14 +1,16 @@
+const int LINES = 4;
+const int PADD = 60;
+const float FONT_SIZE = 1.1f;
+const float PX_PER_CHAR = 20 * FONT_SIZE;
+const int TOTAL_TITLE_CHARS = 10;
 Color blueColor =  new Color(0, 50, 255, 255);
 Color greenColor =  new Color(0, 150, 0, 255);
 Color yellowColor =  new Color(150, 150, 0, 255);
 Color orangeColor =  new Color(150, 75, 0, 255);
 Color redColor =  new Color(150, 0, 0, 255);
-
-const int PADD = 60;
-const int PX_PER_CHAR = 20;
-const int TOTAL_TITLE_CHARS = 10;
 IMyTextSurface screen1;
 IMyTextSurface screen2;
+IMyTextSurface screen3;
 IMyAirVent airVent;
 List <IMyBatteryBlock> batteries = new List<IMyBatteryBlock>();
 List <IMyGasTank> tanks = new List<IMyGasTank>();
@@ -22,10 +24,14 @@ public Program() {
     IMyTextSurfaceProvider block2 = GridTerminalSystem.GetBlockWithName("FSS - Commander Control Chair") as IMyTextSurfaceProvider;
     screen2 = block2.GetSurface(0);
 
+    IMyTextSurfaceProvider block3 = GridTerminalSystem.GetBlockWithName("Command LCD Left") as IMyTextSurfaceProvider;
+    screen3 = block3.GetSurface(0);
+
     airVent = GridTerminalSystem.GetBlockWithName("FSS - Air Vent") as IMyAirVent;
 
     PrepareTextSurfaceForSprites(screen1);
     PrepareTextSurfaceForSprites(screen2);
+    PrepareTextSurfaceForSprites(screen3);
 }
 
 public void Save() {}
@@ -34,9 +40,11 @@ public void Main(string argument, UpdateType updateSource) {
     float airRate = airVent.GetOxygenLevel();
     float energyRate = GetEnergyLevel();
     float oxygenRate = GetOxygenLevel();
+    float hydrogenRate = GetHydrogenLevel();
 
-    DrawRates(screen1, airRate, energyRate, oxygenRate);
-    DrawRates(screen2, airRate, energyRate, oxygenRate);
+    DrawRates(screen1, airRate, energyRate, oxygenRate, hydrogenRate);
+    DrawRates(screen2, airRate, energyRate, oxygenRate, hydrogenRate);
+    DrawRates(screen3, airRate, energyRate, oxygenRate, hydrogenRate);
 }
 
 private float GetEnergyLevel() {
@@ -67,15 +75,31 @@ private float GetOxygenLevel() {
     return current / max;
 }
 
-private void DrawRates(IMyTextSurface screen, float airRate, float energyRate, float oxygenRate) {
+private float GetHydrogenLevel() {
+    GridTerminalSystem.GetBlocksOfType<IMyGasTank>(tanks);
+
+    float max = 0;
+    float current = 0;
+    foreach (IMyGasTank tank in tanks) {
+        if (tank.DisplayNameText.Contains("Hydrogen Tank")) {
+            max += tank.Capacity;
+            current += tank.Capacity * (float)tank.FilledRatio;
+        }
+    }
+
+    return current / max;
+}
+
+private void DrawRates(IMyTextSurface screen, float airRate, float energyRate, float oxygenRate, float hydrogenRate) {
     var frame = screen.DrawFrame();
 
     Echo(screen.Name);
     Echo(GetMessage("   Air Pressure: ", airRate));
     Echo(GetMessage("   E:  ", energyRate));
     Echo(GetMessage("   O2:  ", oxygenRate));
+    Echo(GetMessage("   H2:  ", hydrogenRate));
 
-    DrawSprites(ref frame, screen, airRate, energyRate, oxygenRate);
+    DrawSprites(ref frame, screen, airRate, energyRate, oxygenRate, hydrogenRate);
 
     frame.Dispose();
 }
@@ -84,17 +108,21 @@ private string GetMessage(string title, float rate) {
     return title + ((int)(rate * 100)).ToString() + "%";
 }
 
-private void DrawSprites(ref MySpriteDrawFrame frame, IMyTextSurface screen, float airRate, float energyRate, float oxygenRate) {
+private void DrawSprites(ref MySpriteDrawFrame frame, IMyTextSurface screen, float airRate, float energyRate, float oxygenRate, float hydrogenRate) {
     RectangleF viewport =  GetViewPort(screen);
+    float lineGap = viewport.Height / LINES;
 
     var position = new Vector2(20, 20) + viewport.Position;
     frame.Add(GetLine("Air", airRate, position, viewport));
     
-    position += new Vector2(0, 80);
+    position += new Vector2(0, lineGap);
     frame.Add(GetLine("E", energyRate, position, viewport));
 
-    position += new Vector2(0, 80);
+    position += new Vector2(0, lineGap);
     frame.Add(GetLine("O2", oxygenRate, position, viewport));
+
+    position += new Vector2(0, lineGap);
+    frame.Add(GetLine("H2", hydrogenRate, position, viewport));
 }
 
 private MySprite GetLine(string title, float rate, Vector2 position, RectangleF viewport) {
@@ -104,7 +132,7 @@ private MySprite GetLine(string title, float rate, Vector2 position, RectangleF 
         Type = SpriteType.TEXT,
         Data = data,
         Position = position,
-        RotationOrScale = 1f,
+        RotationOrScale = FONT_SIZE,
         Color = GetColor(rate),
         Alignment = TextAlignment.LEFT,
         FontId = "White"
